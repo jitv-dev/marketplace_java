7ba410f2fc8175c97ce42c98ebd3daae63ebbec5
jitv-dev <javier.torresvalenz@gmail.com>
2026-03-25 22:35:37 -0300

    repositories

--- src/main/java/repository/ProductoRepository.java
+++ src/main/java/repository/ProductoRepository.java
@@ -1,4 +1,21 @@
 package repository;
 
-public class ProductoRepository {
+import model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    List<Producto> findByActivoTrue(Boolean activo);
+
+    List<Producto> findByCategoriaAndActivoTrue(String categoria, Boolean activo);
+
+    List<Producto> findByNombreContainingIgnoreCaseAndActivoTrue(String nombre, Boolean activo);
+
+    @Query("SELECT DISTINCT p.categoria FROM Producto p WHERE p.activo = true ORDER BY  p.categoria")
+    List<String> findCategoriasDistintas();
+
+    @Query("SELECT p FROM Producto p WHERE p.stock <= 5 AND p.activo = true")
+    List<String> findStockBajo();
 }
--- src/main/java/repository/UsuarioRepository.java
+++ src/main/java/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
-public class UsuarioRepository {
+import model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByUsername(String username);
+    boolean existsByEmail(String email);
 }
